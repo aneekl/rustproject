147c26e95567efddc30ccd8c1a64fd0a5e6829c1
aneekl <aneekl014@hotmail.com>
2023-05-26 11:41:00 +0700

    Create Fibonacci.rs

---
+++ Fibonacci.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    // for int in 0..15 {
+    //     println!("fibo ({}) => {}", int, fib(int))
+    // }
+    loop {
+        println!("enter number");
+
+        let mut fibnum = String::new();
+
+        io::stdin()
+            .read_line(&mut fibnum)
+            .expect("Failed to read line");
+
+        let fibnum: u64 = match fibnum.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if fibnum <= 0 {
+            println!("bad number bad bad");
+            break;
+        }
+
+        println!("Fibonacci of {} is {}", fibnum, fib(fibnum));
+        break;
+    }
+}
+
+fn fib(n: u64) -> u64 {
+    if n <= 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
